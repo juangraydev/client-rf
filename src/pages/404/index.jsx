73850d43f7000cc2e831328701633618730b261d
Jhun Wulf Sabala <jhun.sabala@lanexcorp.com>
2025-11-08 10:47:05 +0800

    fix typo error

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -10,7 +10,7 @@ export default function Error404() {
     <main className="relative min-h-screen flex  text-white max-h-[calc(100vh - 150px)]">
       <div className="h-[calc(100vh-202px)] w-full mt-[140px] z-10 px-[1rem] relative">
         <div className="text-left leading-[0.8] absolute top-[calc(45%-120px)] left-[5%]">
-          <h1 className="text-[64px] font-[800] text-[#FFF]">ERORR 404</h1>
+          <h1 className="text-[64px] font-[800] text-[#FFF]">ERROR 404</h1>
           <h1 className="text-[45px] font-[800] text-[#FBAA0F]">Page Not Found!</h1>
         </div>
       </div>
